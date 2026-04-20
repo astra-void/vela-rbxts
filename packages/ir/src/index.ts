ad7a33525d8cba82534b8d61592fe220af66166a
astra-void <dev@astra-void.xyz>
2026-04-21 08:36:47 +0900

    feat(ir): define StyleIR and TransformResult types

--- packages/ir/src/index.ts
+++ packages/ir/src/index.ts
@@ -1,2 +1,28 @@
-// TODO: Add intermediate representation contracts.
-export const IR_PLACEHOLDER = "ir-placeholder";
+export type Diagnostic = {
+	level: "warning" | "error";
+	code: string;
+	message: string;
+	token?: string;
+};
+
+export type PropEntry = {
+	name: string;
+	value: string;
+};
+
+export type HelperEntry = {
+	tag: string;
+	props: PropEntry[];
+};
+
+export type StyleIR = {
+	props: PropEntry[];
+	helpers: HelperEntry[];
+	diagnostics: Diagnostic[];
+};
+
+export type TransformResult = {
+	code: string;
+	diagnostics: Diagnostic[];
+	changed: boolean;
+};
