04a76d87106fc4acd32361d7150e939ee788b558
astra-void <dev@astra-void.xyz>
2026-04-22 23:26:53 +0900

    feat(vela-rbxts): introduce renamed main package

---
+++ packages/vela-rbxts/src/index.ts
@@ -0,0 +1,23 @@
+import type { ClassValue } from "@rbxts-tailwind/types";
+
+export {
+	defaultConfig,
+	defineConfig,
+	type TailwindConfig,
+	type TailwindConfigInput,
+} from "@rbxts-tailwind/config";
+
+export { createRbxtscTransformerBridge as createTransformer } from "@rbxts-tailwind/rbxtsc-host";
+
+export type {
+	ClassValue,
+	StylableProps,
+} from "@rbxts-tailwind/types";
+
+declare global {
+	namespace React {
+		interface Attributes {
+			className?: ClassValue;
+		}
+	}
+}
